5620124ba49093fe97def2d13ece2df08806e467
Luca Bertelli <luca.bertelli@dfinity.org>
2025-08-13 22:45:47 +0200

    fix: skip github zipball top level directory (#50)

--- scripts/utils/fs.ts
+++ scripts/utils/fs.ts
@@ -9,9 +9,29 @@ export async function syncFile(
   await sync(srcFile, destFile);
 }
 
+export async function syncDir(srcDir: string, destDir: string): Promise<void> {
+  await fs.ensureDir(destDir);
+  await sync(srcDir, destDir);
+}
+
 async function sync(src: string, dest: string): Promise<void> {
   await fs.copy(src, dest, {
     overwrite: true,
     preserveTimestamps: true,
   });
+}
+
+export async function topLevelDir(dir: string): Promise<string> {
+  const topLevelDirs: string[] = [];
+  for await (const entry of Deno.readDir(dir)) {
+    if (entry.isDirectory) {
+      topLevelDirs.push(entry.name);
+    }
+  }
+  if (topLevelDirs.length === 1) {
+    return topLevelDirs[0];
+  }
+  throw new Error(
+    `Expected 1 top-level directory in ${dir}, got ${topLevelDirs.length}`,
+  );
 }
