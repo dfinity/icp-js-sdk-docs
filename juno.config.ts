e4551f8ebc3bc5d45c29301b6f23462fe35f4d99
Luca Bertelli <luca.bertelli@dfinity.org>
2025-08-27 13:46:49 +0200

    fix: upload both compressed and uncompressed html files (#83)
    Bumps juno deps in order to implement the fix.

--- juno.config.ts
+++ juno.config.ts
@@ -14,10 +14,23 @@ export default defineConfig({
       redirects: REDIRECTS,
       rewrites: REWRITES,
     },
-    precompress: {
-      mode: "replace", // only upload the compressed version of the file
-      algorithm: "brotli",
-    },
+    precompress: [
+      {
+        pattern: "**/*.+(js|mjs|css)",
+        algorithm: "brotli",
+        // only upload the compressed version for these files
+        // since all browsers support this encoding, it reduces
+        // upload time and saves heap memory
+        mode: "replace",
+      },
+      {
+        pattern: "**/*.html",
+        algorithm: "brotli",
+        // upload both the compressed and uncompressed version of HTML files
+        // to avoid issues with OpenGraph previews
+        mode: "both",
+      },
+    ],
   },
   emulator: {
     satellite: {},
