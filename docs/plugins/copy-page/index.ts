21feda1177d2d6de5b9e6fffbb423b376f075252
Luca Bertelli <luca.bertelli@dfinity.org>
2025-11-19 14:45:16 +0100

    feat: copy page button (#150)
    Adds "Copy Page" button to each docs page.
    
    Follow-up of #148.

--- docs/plugins/copy-page/index.ts
+++ docs/plugins/copy-page/index.ts
@@ -8,10 +8,17 @@ export function copyPagePlugin(): StarlightPlugin {
       "config:setup": (ctx) => {
         ctx.addIntegration(
           copyPageIntegration({
-            siteUrl: ctx.astroConfig.site!, // we assume this is set
+            siteUrl: ctx.astroConfig.site!, // we assume this is always set
             logger: ctx.logger,
           }),
         );
+
+        ctx.updateConfig({
+          components: {
+            ...ctx.config.components,
+            PageTitle: "./plugins/copy-page/components/PageTitle.astro",
+          },
+        });
       },
     },
   };
